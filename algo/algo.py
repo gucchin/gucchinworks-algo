25dbfc7fa444be42aa30f49745b70a2b6dd11b24
takashi <goodtimefriends@gmail.com>
2019-01-05 21:07:44 +0900

    emaを追加。maをsmaに名前を変更

--- algo/algo.py
+++ algo/algo.py
@@ -1,14 +1,42 @@
+# -*- coding : utf-8
+"""
+copyright 2019 gucchin all right reserved.
+"""
+
 import numpy as np
 import pandas as pd
 
 
-def ma(inClose, inPeriod):
-    return inClose.rolling(inPeriod).mean()
+def sma(inClose, inPeriod=9):
+    """sma is simple moving average"""
+    return inClose.rolling(inPeriod, min_periods=1).mean()
+
+
+def ema(inClose, inPeriod=9):
+    """ema is exponential moving average"""
+    s = sma(inClose, inPeriod)
+
+    e = np.zeros_like(s)
+    e[:inPeriod] = s[:inPeriod]
+
+    for i in range(inPeriod, len(s)):
+        e[i] = e[i - 1] + (inClose[i] - e[i - 1]) * 2.0 / (inPeriod + 1)
+
+    return pd.Series(data=e, index=inClose.index)
+
+
+def macd(inClose, shortPeriod=12, longPeriod=26, signalPeriod=9):
+    """macd is moving average convergence divergence"""
+    mac = ema(inClose, shortPeriod) - ema(inClose, longPeriod)
+    sig = ema(mac, signalPeriod)
+    return mac, sig
+
 
 def bband(inClose, inPeriod, n):
+    """bband is bollinger bands"""
     std = inClose.rolling(inPeriod).std()
 
-    mid = ma(inClose, inPeriod)
+    mid = sma(inClose, inPeriod)
     high = mid + n*std
     low = mid - n*std
 
@@ -16,8 +44,7 @@ def bband(inClose, inPeriod, n):
 
 
 def hv(inClose, inPeriod=20, convert=252):
+    """hv is historical volatility"""
     x = inClose.shift(1)
-
     s = np.log(inClose / x)
-    print(s.tail())
     return s.rolling(inPeriod).std(ddof=1) * 100 * np.sqrt(convert)
